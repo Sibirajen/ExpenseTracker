eaa83d9b7b3488e34959974a891b745758888b47
sibirajen <sibirajen16@gmail.com>
2025-02-12 15:06:44 +0530

    Added Expense.java

---
+++ src/main/java/com/sibirajen/Expense.java
@@ -0,0 +1,42 @@
+package com.sibirajen;
+
+import java.time.LocalDate;
+
+public class Expense {
+    private int id;
+    private LocalDate date;
+    private String description;
+    private double amount;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
