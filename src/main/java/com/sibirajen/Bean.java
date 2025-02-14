e2256c735d8d23f069897a734d1fbfab60dc084d
sibirajen <sibirajen16@gmail.com>
2025-02-14 17:08:45 +0530

    Added Bean Class

---
+++ src/main/java/com/sibirajen/Bean.java
@@ -0,0 +1,37 @@
+package com.sibirajen;
+
+
+import com.opencsv.bean.CsvBindByPosition;
+
+public class Bean {
+    @CsvBindByPosition(position = 0)
+    private int id;
+    @CsvBindByPosition(position = 1)
+    private String name;
+    @CsvBindByPosition(position = 2)
+    private double Amount;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getAmount() {
+        return Amount;
+    }
+
+    public void setAmount(double amount) {
+        Amount = amount;
+    }
+}
