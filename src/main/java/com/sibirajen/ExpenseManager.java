f9861afecbfcd1f5e31f6fcc33ebf709be82f98c
sibirajen <sibirajen16@gmail.com>
2025-02-16 16:34:59 +0530

    Added addExpense and getUniqueId methods

--- src/main/java/com/sibirajen/ExpenseManager.java
+++ src/main/java/com/sibirajen/ExpenseManager.java
@@ -10,12 +10,30 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class ExpenseManager {
-    private List<Expense> expenseList;
+    private final List<Expense> expenseList;
+    private int uniqueId;
 
     private static final Logger logger = Logger.getLogger(ExpenseManager.class.getName());
 
     public ExpenseManager() {
         this.expenseList = loadExpense();
+        this.uniqueId = getUniqueId();
+    }
+
+    public void addExpense(String name, double amount){
+        int id = ++uniqueId;
+        Expense expense = new Expense(id, name, amount);
+        expenseList.add(expense);
+        System.out.println("Expense added successfully (ID: " + id + " )");
+    }
+
+    private int getUniqueId(){
+        int size = expenseList.size();
+        if(expenseList.isEmpty()){
+            return 99;
+        }
+        Expense expense = expenseList.get(size-1);
+        return expense.getId();
     }
 
     private List<Expense> loadExpense() {
@@ -29,7 +47,7 @@ public class ExpenseManager {
 
     public void saveExpense(){
         try {
-            CsvHandler.writeCsv(this.expenseList);
+            CsvHandler.writeCsv(expenseList);
         } catch (CsvRequiredFieldEmptyException | CsvDataTypeMismatchException | IOException e) {
             logger.log(Level.SEVERE, "Error occurred while writing the CSV file: " + e.getClass().getSimpleName());
         }
