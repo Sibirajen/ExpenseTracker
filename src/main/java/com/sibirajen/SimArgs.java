29a5e7db173bd4ce83872d332cfe06ababbaafd0
sibirajen <sibirajen16@gmail.com>
2025-02-18 14:42:02 +0530

    Added test cases

--- src/main/java/com/sibirajen/SimArgs.java
+++ src/main/java/com/sibirajen/SimArgs.java
@@ -4,6 +4,7 @@ public class SimArgs {
     public static void main(String[] args) {
         String[][] commands = {
                 {},
+                {"abc"},
                 {"list"},
                 {"add"},
                 {"add","Lunch","20"},
@@ -11,6 +12,15 @@ public class SimArgs {
                 {"add","-d","Lunch","-a","abc"},
                 {"add","-d","Lunch","-a","-20"},
                 {"add","-d","Lunch","-a","20"},
+                {"add","-d","Snacks","-a","10"},
+                {"add","-d","Ticket","-a","30"},
+                {"list"},
+                {"delete"},
+                {"delete","100"},
+                {"delete","-i","abc"},
+                {"delete","--i","100"},
+                {"delete","-i","100"},
+                {"delete","-i","100"},
                 {"list"}
         };
 
