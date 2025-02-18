f438b769afdb4e4f043532c6d8b19c70ea875280
sibirajen <sibirajen16@gmail.com>
2025-02-18 15:59:55 +0530

    Improved ValidateCmd code

--- src/main/java/com/sibirajen/ValidateCmd.java
+++ src/main/java/com/sibirajen/ValidateCmd.java
@@ -3,7 +3,7 @@ package com.sibirajen;
 public class ValidateCmd {
     public static boolean checkAddCmd(String[] commands) {
         if (commands.length != 5 || !commands[1].equals("-d") || !commands[3].equals("-a")) {
-            System.out.println("Usage: java ExpTracker add -d [argument] -a [argument]");
+            System.out.println("Usage: java ExpTracker add -d <description> -a <amount>");
             return false;
         }
 
@@ -12,16 +12,7 @@ public class ValidateCmd {
 
     public static boolean checkDelCmd(String[] commands) {
         if (commands.length != 3 || !commands[1].equals("-i")) {
-            System.out.println("Usage: java ExpTracker delete -i [argument]");
-            return false;
-        }
-
-        return true;
-    }
-
-    public static boolean checkUpdateCmd(String[] commands) {
-        if (commands.length != 3 || !commands[1].equals("-i")) {
-            System.out.println("Usage: java ExpTracker update -i [argument]");
+            System.out.println("Usage: java ExpTracker delete -i <expense_id>");
             return false;
         }
 
@@ -30,7 +21,7 @@ public class ValidateCmd {
 
     public static boolean checkSummaryCmd(String[] commands) {
         if (commands.length != 3 || !commands[1].equals("-m")) {
-            System.out.println("Usage: java ExpTracker summary -m [argument]");
+            System.out.println("Usage: java ExpTracker summary -m <month>");
             return false;
         }
 
